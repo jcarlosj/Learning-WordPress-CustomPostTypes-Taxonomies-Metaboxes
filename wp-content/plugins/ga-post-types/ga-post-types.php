785791d0d7caf4a8a2181d88ebf89a7a156ebfad
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-02-16 12:58:03 -0500

    Crea 'Custom Post Type' Avanzado (Eventos) dentro del 'Plugin'

--- wp-content/plugins/ga-post-types/ga-post-types.php
+++ wp-content/plugins/ga-post-types/ga-post-types.php
@@ -52,6 +52,7 @@ function crear_post_type_recetas() {
     'show_in_menu'        => true,          // [true/false] Mostrar o no publicación en el menú de administración (que se vea en el menu lateral en el admin)
     'show_in_admin_bar'   => true,          // [true/false] Mostrar o no publicación en la barra de administración
     'menu_position'       => 6,             // Posición en el menú lateral en el admin (Orden de aparición del item en el menú) 'show_in_menu' debe ser true
+    'menu_icon'           => 'dashicons-book-alt',  // Agrega ícono al ítem de menú lateral en el admin (Usando DashIcons de WordPress en este caso)
     'can_export'          => true,          // [true/false] habilitar o deshabilitar la exportación como XML para importarlo en otro lugar
     'has_archive'         => true,          // [true/false] habilitar o deshabilitar el uso del archivo 'archive.php'
     'exclude_from_search' => false,         // [true/false] habilitar o deshabilitar posibilidad de busqueda de contenidos en el buscador de WordPress
@@ -70,6 +71,71 @@ function crear_post_type_recetas() {
 add_action(
   'init',                                     // Lugar donde queremos que se ejecute la funcionalidad
   'crear_post_type_recetas',                  // La funcionalidad o código a desplegar
+  0                                           // 0 (cero): Prioridad de ejecución para que se ejecute primero
+);
+/* NOTA: Se establecen prioridades cuando se usa un Hook para asociar dos funciones (funcionalidades) */
+
+// Crea un post type 'Eventos'
+function crear_post_type_eventos() {
+
+  // Array de etiquetas personalizadas para el 'Post Type'
+  $labels = array(
+      'name'                => _x( 'Eventos', 'Post Type General Name', 'gourmet-artist' ),         // Nombre del post type (para la vista)
+      'singular_name'       => _x( 'Evento', 'Post Type Singular Name', 'gourmet-artist' ),
+      'menu_name'           => __( 'Eventos', 'gourmet-artist' ),
+      'parent_item_color'   => __( 'Evento Padre', 'gourmet-artist' ),
+      'all_items'           => __( 'Todos las Eventos', 'gourmet-artist' ),
+      'view_item'           => __( 'Ver Evento', 'gourmet-artist' ),
+      'add_new_item'        => __( 'Agregar Nuevo Evento', 'gourmet-artist' ),
+      'add_new'             => __( 'Agregar Nuevo Evento', 'gourmet-artist' ),
+      'edit_item'           => __( 'Editar Evento', 'gourmet-artist' ),
+      'update_item'         => __( 'Actualizar Evento', 'gourmet-artist' ),
+      'search_items'        => __( 'Buscar Evento', 'gourmet-artist' ),
+      'not_found'           => __( 'Evento no encontrada', 'gourmet-artist' ),
+      'not_found_in_trash'  => __( 'Evento no encontrada en la papelera', 'gourmet-artist' )
+  );
+  /* NOTA: se agrega 'gourmet-artist' en cada una de las etiquetas, por que es importante si se va a usar la 'internacionalización del theme de WP' */
+
+  // Más opciones para la personalización del 'Post Type'
+  $args = array(
+    'label'         => __( 'eventos', 'gourmet-artist' ),               // Nombre de la etiqueta del post type
+    'description'   => __( 'Eventos de la empresa', 'gourmet-artist' ),
+    'labels'        => $labels,                                         // Etiquetas personalizadas para el 'Post Type' definidas en la variable $labels
+    'supports'      => array(                                           // Todas las características que soportará el 'Custom Post Type'
+        'title',            // Campo de Título
+        'editor',           // Campo para editar contenido tradicional del CPT
+        'excerpt',          // Campo de Descripción corta
+        'author',           // Campo de Autor
+        'thumbnail',        // Campo de imágen destacada
+        'comments',         // Campo para comentarios
+        'revisions',        // Versionamiento de contenidos del CPT
+        'custom-fields'     // Campos personalizados
+    ),
+    'hierarchical'        => false,                                           // [true/false] habilitar o deshabilitar el comportamiento jerarquico (padres e hijos) su comportamiento sería como el de una página (page)
+    'public'              => true,          // El 'Post Type' va a ser público
+    'show_ui'             => true,          // [true/false] Genera y permite o no una interfaz de usuario para administrar esta publicación en el administrador (que se vea la interfaz)
+    'show_in_menu'        => true,          // [true/false] Mostrar o no publicación en el menú de administración (que se vea en el menu lateral en el admin)
+    'show_in_admin_bar'   => true,          // [true/false] Mostrar o no publicación en la barra de administración
+    'menu_position'       => 6,             // Posición en el menú lateral en el admin (Orden de aparición del item en el menú) 'show_in_menu' debe ser true
+    'menu_icon'           => 'dashicons-calendar-alt',  // Agrega ícono al ítem de menú lateral en el admin (Usando DashIcons de WordPress en este caso)
+    'can_export'          => true,          // [true/false] habilitar o deshabilitar la exportación como XML para importarlo en otro lugar
+    'has_archive'         => true,          // [true/false] habilitar o deshabilitar el uso del archivo 'archive.php'
+    'exclude_from_search' => false,         // [true/false] habilitar o deshabilitar posibilidad de busqueda de contenidos en el buscador de WordPress
+    'capability_type'     => 'page'         // Capacidad construir capacidades de lectura, edición y eliminación (se puede pasar como 'Array', para permitir plurales alternativos)
+                                            // en este caos se le pasa un tipo de capacidad definida en WordPress 'page', aunque por defecto si no se definen por defecto tomará 'post'
+  );
+
+  // 'register_post_type' Crea y registra un post type en WP
+  register_post_type(
+    'eventos',          // Nombre del identificador del post type (para la WP)
+    $args               // Los argumentos de personalización del CPT
+  );
+
+}
+// Hook: es la acción que permite identificar una funcionalidad por WP y donde se desea ejecutar
+add_action(
+  'init',                                     // Lugar donde queremos que se ejecute la funcionalidad
+  'crear_post_type_eventos',                  // La funcionalidad o código a desplegar
   0                                           // 0 (cero): Prioridad de ejecución para que se ejecute primero
 );
 /* NOTA: Se establecen prioridades cuando se usa un Hook para asociar dos funciones (funcionalidades) */
