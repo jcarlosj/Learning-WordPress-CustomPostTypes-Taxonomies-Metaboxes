f895c8d329fa182e4fdb47a4f68e8f4ebbe3763c
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-02-16 10:27:57 -0500

    Crea un 'Plugin' personalizado (para nuestros Custom Post Types)

---
+++ wp-content/plugins/ga-post-types/ga-post-types.php
@@ -0,0 +1,12 @@
+<?php
+  /*
+   Plugin Name: Gourmet Artist Post Types
+   Plugin URI:
+   Description: Agrega 'Custom Post Types' al sitio Gourmet Artist
+   Version: 1.0
+   Author: Juan Carlos Jiménez Gutiérrez
+   Author URI:
+   License: GPL2
+   License URI: https://www.gnu.org/licenses/gpl-2.0.html
+   */
+?>
