611400184b197ac84e30a9ab98904bf6fb0c3130
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-02-16 10:47:02 -0500

    Crea 'Custom Post Type' dentro del 'Plugin' personalizado (Recetas)
    Aclaración:
    Se desplegará en el menú del 'Backend' el menú 'Recetas' con funcionalidad
    básica de un 'Custom Post Type'

--- wp-content/plugins/ga-post-types/ga-post-types.php
+++ wp-content/plugins/ga-post-types/ga-post-types.php
@@ -9,4 +9,26 @@
    License: GPL2
    License URI: https://www.gnu.org/licenses/gpl-2.0.html
    */
+
+// Crea un post type 'Recetas'
+function crear_post_type_recetas() {
+  // 'register_post_type' Crea y registra un post type en WP
+  register_post_type(
+    'recetas',                                // Nombre del post type (identificador para WP)
+    array(                                    // Array de etiquetas
+      'labels' => array(
+          'name' => __( 'Recetas' ),          // Nombre del post type (para la vista)
+          'singular_name' => __( 'Receta' )
+      ),
+      'public' => true,                       // El 'Post Type' va a ser público
+      'has_archive' => true,                  // Para que pueda hacer uso del archivo 'archive.php'
+    )
+  );
+}
+// Hook: es la acción que permite identificar una funcionalidad por WP y donde se desea ejecutar
+add_action(
+  'init',                                     // Lugar donde queremos que se ejecute la funcionalidad
+  'crear_post_type_recetas'                   // La funcionalidad o código a desplegar
+);
+
 ?>
