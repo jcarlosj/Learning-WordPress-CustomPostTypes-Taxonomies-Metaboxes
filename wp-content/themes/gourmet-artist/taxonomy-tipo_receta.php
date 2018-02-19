15cbd0c20191b78221490f220cb5740bec4790d5
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-02-19 18:12:56 -0500

    Crea archivo específico para desplegar taxonomía 'tipo_receta' en el archivo 'taxonomy-tipo_receta.php'

---
+++ wp-content/themes/gourmet-artist/taxonomy-tipo_receta.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Gourmet_Artist
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area medium-8 columns">
+		<span class="file">taxonomy-tipo_receta.php</span>
+
+		<main id="main" class="site-main">
+
+		<?php
+		if ( have_posts() ) : ?>
+
+			<header class="page-header">
+				<div class="header-taxonomy-tipo-receta">
+
+					<?php
+						// Sin usar las funciones de 'UnderScore Theme', al estilo WordPress
+						$termino_actual = get_queried_object();													// Obtiene el término Actual
+						$taxonomia      = get_taxonomy( $termino_actual -> taxonomy );	// Obtiene la taxonomía a partir del término actual
+					?>
+					<h1 class="page-title text-center">
+							<span class="archive-description">
+								<?php echo $termino_actual -> name; ?>
+							</span>
+					</h1>
+
+				</div>
+
+				<?php
+				  # Funciones de 'UnderScore Theme'
+					#the_archive_title( '<h1 class="page-title text-center">', '</h1>' );
+					#the_archive_description( '<div class="archive-description">', '</div>' );
+				?>
+			</header><!-- .page-header -->
+
+			<?php
+			/* Start the Loop */
+			while ( have_posts() ) : the_post();
+
+				/*
+				 * Include the Post-Format-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				 */
+				get_template_part( 'template-parts/content', get_post_format() );
+
+			endwhile;
+
+			the_posts_navigation();
+
+		else :
+
+			get_template_part( 'template-parts/content', 'none' );
+
+		endif; ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
