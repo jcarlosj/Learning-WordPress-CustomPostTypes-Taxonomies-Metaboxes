7d8a7e6b1677a1e8792448c28f4d907a888610bd
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-02-23 09:12:11 -0500

    Crea un 'Plugin' personalizado (para nuestros Metaboxes)

---
+++ wp-content/plugins/ga-metaboxes/ga-metaboxes.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ Plugin Name: Gourmet Artist Metaboxes
+ Plugin URI:
+ Description: Agrega 'Metaboxes' al sitio Gourmet Artist
+ Version: 1.0
+ Author: Juan Carlos Jiménez Gutiérrez
+ Author URI:
+ License: GPL2
+ License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
+ 
+?>
