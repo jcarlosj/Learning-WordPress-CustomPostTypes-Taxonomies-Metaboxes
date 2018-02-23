ee778a02aa2a81d3a91843e561bbb6563aaee1f4
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-02-23 13:22:10 -0500

    Implementa plugin 'CMB2' como plugin personalizado
    Aclaración:
    Esta no es la implementación clasica del plugin dentro del directorio
    global del sitio. Esta forma permite que el plugin se encuentre embebido
    dentro de un plugin personalizado e "independiente"

---
+++ wp-content/plugins/ga-CMB2-metaboxes/ga-CMB2-metaboxes.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ Plugin Name: Gourmet Artist CMB2
+ Plugin URI:
+ Description: Agrega plugin CMB2 como plugin personalizado para crear 'Metaboxes' al sitio Gourmet Artist
+ Version: 1.0
+ Author: Juan Carlos Jiménez Gutiérrez
+ Author URI:
+ License: GPL2
+ License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+?>
