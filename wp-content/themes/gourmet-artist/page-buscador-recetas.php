273c1273a6845d3457a7d7b2c0f50ba0b31a260a
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-02-24 15:30:22 -0500

    Despliega los terminos para crear un menú para filtrar la búsqueda

--- wp-content/themes/gourmet-artist/page-buscador-recetas.php
+++ wp-content/themes/gourmet-artist/page-buscador-recetas.php
@@ -17,10 +17,25 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-		
+			<?php
+				# Obtenemos los terminos de una taxonomía específica 'tipo_receta'
+				$terminos = get_terms(
+					array(
+						'taxonomy' => 'tipo_receta'
+					),
+					array(
+						'hide_empty' => false					// [true/false] Oculta o NO los términos de la taxonomía que están vacías
+					)
+				);
+			?>
+			<ul class="simplefilter menu row">
+				<?php foreach ( $terminos as $key => $termino ): ?>
+					<?php echo '<li data-filter="' .$termino -> term_taxonomy_id. '">' .$termino -> name. '</li>'; ?>
+				<?php endforeach; ?>
+			</ul>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
+	
 <?php
 get_footer();
