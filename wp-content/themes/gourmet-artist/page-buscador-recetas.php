546697b5e087ddc1aef798bb48b4f8b0bab0a71a
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-02-24 15:42:59 -0500

    Despliega los títulos de todos los Post Types (Recetas) 'page-buscador-recetas.php'

--- wp-content/themes/gourmet-artist/page-buscador-recetas.php
+++ wp-content/themes/gourmet-artist/page-buscador-recetas.php
@@ -28,14 +28,32 @@ get_header(); ?>
 					)
 				);
 			?>
-			<ul class="simplefilter menu row">
+			<ul class="simplefilter menu">
 				<?php foreach ( $terminos as $key => $termino ): ?>
 					<?php echo '<li data-filter="' .$termino -> term_taxonomy_id. '">' .$termino -> name. '</li>'; ?>
 				<?php endforeach; ?>
 			</ul>
 
+			<?php
+
+			# Creamos los argumentos de la consulta que deseamos hacer a WordPress
+      $args = array(
+        'post_type'      => 'recetas',   				# Nombre del 'Post Type' (Se puede ver en la URL del ADMIN)
+        'posts_per_page' => -1,                 # Cantidad de registros a mostrar por página (-1 significa imprimirlos todos
+        'orderby'        => 'title',            # Ordenar por: Fecha de publicación, orden alfabético, Author etc.
+        'order'          => 'ASC'              # Tipo de orden: Ascendente, Descendente
+      );
+      #
+      $recetas = new WP_Query( $args );          # Hacemos la consulta usando el 'WP_Query' y pasamos los argumentos de la misma
+      while ( $recetas -> have_posts() ): $recetas -> the_post();    # Creamos un loop para imprimir los valores traidos por la consulta
+    ?>
+
+      <?php the_title(); ?><br />
+
+    <?php endwhile; wp_reset_postdata(); # Solo usamos 'wp_reset_postdata()' cuando se use el 'WP_Query()' ?>
+
 		</main><!-- #main -->
 	</div><!-- #primary -->
-	
+
 <?php
 get_footer();
